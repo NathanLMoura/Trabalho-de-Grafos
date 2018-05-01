56b1e94c626136b948f234572097778a3240241e
NathanLmoura <nathan.cel@hotmail.com>
2018-05-01 10:32:00 -0300

    Desenvolvimento da Classe Grafo em Matriz de Adjacencia

--- Trabalho-de-Grafos/Classes/GrafoMA/GrafoMA.cs
+++ Trabalho-de-Grafos/Classes/GrafoMA/GrafoMA.cs
@@ -57,7 +57,7 @@ namespace Trabalho_de_Grafos.Classes.GrafoMA
             int contGrau = 0;
             for (int c = 0; c < qtVertices; c++)
             {
-                contGrau += MA[c, qtVertices];
+                contGrau += MA[c, vertice];
             }
             return contGrau;
         }
@@ -66,9 +66,13 @@ namespace Trabalho_de_Grafos.Classes.GrafoMA
         {
             int contAres = 0;
             int compVert = ((qtVertices * (qtVertices - 1)) / 2);
-            for (int c = 0; c < qtVertices; c++)
+            for (int i = 0; i < qtVertices; i++)
             {
-                contAres = Grau(c);//Função não esta finalizada, precisa ser corrigida
+                for(int j = i; j < qtVertices; j++)
+                {
+                    contAres += MA[i, j];
+                }
+                
             }
             if (contAres == compVert)
             {
@@ -82,45 +86,105 @@ namespace Trabalho_de_Grafos.Classes.GrafoMA
 
         public bool Regular()
         {
-            throw new System.NotImplementedException();
+            int grauAnterior = Grau(0);
+            for (int c = 1; c < qtVertices; c++)
+            {
+                int grauAtual = Grau(c);
+
+                if(grauAnterior != grauAtual)
+                {
+                    return false;
+                }
+                grauAnterior = grauAtual;
+
+            }
+            return true;
         }
 
         public void SequenciaGraus()
         {
-            throw new System.NotImplementedException();
+            int[] sequenciaGraus = new int[qtVertices];
+            for (int c = 0; c < qtVertices; c++)
+            {
+                sequenciaGraus[c] = Grau(c);
+            }
+
+
+            int auxInt=0;
+            for(int i=0; i < qtVertices; i++)
+            {
+                for(int j = 0; j < qtVertices; j++)
+                {
+                    if (sequenciaGraus[i] < sequenciaGraus[j])
+                    {
+                        auxInt = sequenciaGraus[i];
+                        sequenciaGraus[i] = sequenciaGraus[j];
+                        sequenciaGraus[j] = auxInt;
+                    }
+                }
+            }
         }
 
         public void VerticesAdjacentes(int vertice)
         {
-            throw new System.NotImplementedException();
+            Console.Write(vertice + ":");
+            for (int c = 0; c < qtVertices; c++)
+            {
+                if (MA[vertice, c] == 1)
+                    Console.Write(" " + c + ",");
+            }
         }
 
         public bool Isolado(int vertice)
         {
-            throw new System.NotImplementedException();
+            for (int c = 0; c < qtVertices; c++)
+            {
+                if (Grau(c) == 0)
+                    return true;
+            }
+            return false;
         }
 
         public bool Impar(int vertice)
         {
-            throw new System.NotImplementedException();
+            if (Grau(vertice) % 2 != 0)
+                return true;
+            return false;
         }
 
         public bool Par(int vertice)
         {
-            throw new System.NotImplementedException();
+            if (Grau(vertice) % 2 == 0)
+                return true;
+            return false;
         }
 
         public bool Adjacentes(int v1, int v2)
         {
-            throw new System.NotImplementedException();
+            if (MA[v1, v2] == 1)
+                return true;
+            return false;
+
         }
         public void ShowMA()
         {
-            throw new System.NotImplementedException();
+            for(int i = 0; i < qtVertices; i++)
+            {
+                Console.Write(i + ":");
+                for (int j = 0; j < qtVertices; j++)
+                {
+                    Console.Write(" " + MA[i, j] + ",");
+                }
+                Console.Write("\n");
+            }
         }
         public void ShowLA()
         {
-            throw new System.NotImplementedException();
+            for(int c = 0; c < qtVertices; c++)
+            {
+                VerticesAdjacentes(c);
+                Console.Write("\n");
+            }
         }
     }
 }
